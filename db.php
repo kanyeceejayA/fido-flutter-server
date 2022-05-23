2aad830417834b931f03f5686bb3e845923fdc10
kanyeceejayA <kakbr800@gmail.com>
2022-05-23 14:05:52 +0300

    update env key

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
+include('env.php');
 echo 'Wroking <br>';
 ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting('E_ALL');
-include('env.php');
 
 $dns = 'mysql:host=localhost;dbname=silbaka_fido';
 $user = 'silbaka_fidoUser';
-$password = env('password');
+$password = env('dbPassword');
 try{
  $db = new PDO ($dns, $user, $password);
  echo '<h1>success</h1>';
