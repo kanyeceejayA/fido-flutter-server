d68d3425ae013f685ea963fcef28be2ab9c2fc2e
kanyeceejayA <kakbr800@gmail.com>
2022-05-23 22:28:09 +0300

    add read.php

---
+++ read.php
@@ -0,0 +1,39 @@
+<?php 
+require('env.php');
+
+$servername = "localhost";
+$dbname = env('dbName');
+$username = env('dbUser');
+$password = env('dbPassword');
+
+try {
+    if (empty($_GET['key']) ){ http_response_code(412);echo null; return;}
+    $key = $_GET['key'];
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // use exec() because no results are returned
+    // $conn->exec($sql);
+    
+    $stmt = $conn->prepare("SELECT content FROM fidodata  WHERE datakey = :key");
+    $stmt->bindParam(':key', $key);
+    
+    $stmt->execute();
+
+    if ($stmt->rowCount() == 0){http_response_code(412);return;}
+
+    foreach ($stmt as $row) {
+        $content = $row['content'];
+    }
+    http_response_code(200);
+    echo $content;
+
+    } catch(PDOException $e) {
+        http_response_code(400);
+       echo $e->getMessage();
+    }catch(Error $e){
+        echo $e;
+    }
+    
+    $conn = null;
+    
