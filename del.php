0afd294613585aa2434f13c77d92ae24c6e568dd
kanyeceejayA <kakbr800@gmail.com>
2022-05-23 22:15:46 +0300

    add del and new changed

---
+++ del.php
@@ -0,0 +1,42 @@
+<?php 
+require('env.php');
+
+$servername = "localhost";
+$dbname = env('dbName');
+$username = env('dbUser');
+$password = env('dbPassword');
+
+try {
+    if (empty($_GET['key']) || empty($_GET['d'])){ http_response_code(412);echo null; return;}
+    $key = $_GET['key'];
+    $content = $_GET['d'];
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO fidodata (datakey, content)
+            VALUES ('$key', '$content')";
+    // use exec() because no results are returned
+    // $conn->exec($sql);
+    
+    $stmt = $conn->prepare("INSERT INTO fidodata (datakey, content)
+        VALUES (:key, :content)
+        ON DUPLICATE KEY UPDATE
+        datakey = values(datakey),
+        content = values(content)");
+    $stmt->bindParam(':key', $key);
+    $stmt->bindParam(':content', $content);
+
+    $stmt->execute();
+
+    http_response_code(200);
+    echo 'Ok';
+
+    } catch(PDOException $e) {
+        http_response_code(400);
+       echo $e->getMessage();
+    }catch(Error $e){
+        echo $e;
+    }
+    
+    $conn = null;
+    
--- get.php
+++ get.php
@@ -20,28 +20,6 @@ function registrationRequest(String $userId = null)
 
 }
 
-Future<Map<String,dynamic>> registrationRequest({required String userId}) async{
-    if(userId == '') throw PlatformException(code: 'User_Name_Cannot_Be_Blank',message: 'Username cannot be blank');
-    String challenge = _generateChallenge();
-
-    var _data = (await _storage.read(key: userId)) ??'{"credentials":[]}';
-    // [
-    // 	{"credentialId": credentialId, "publicKey": publicKey}, 
-    // 	{"credentialId": credentialId, "publicKey": publicKey}
-    // ]
-    List credentials = (json.decode(_data))['credentials'];
-    List ids = [];
-    for (var item in credentials) {
-      ids.add(item['credentialId']);
-    }
-
-    await _storage.write(key: 'RequestChallengeFor'+userId,value: challenge);
-
-    print  ({'challenge': challenge,'credentials':ids});
-    return {'challenge': challenge,'credentials':ids};
-}
-
-
 function generateChallenge($l=32){
     return substr(str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._"), 10, $l);
  }
